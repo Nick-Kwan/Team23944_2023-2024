b97e7e23157639ecea787b96de8e371a1e00b756
Alex-S-09 <aeshelton09@gmail.com>
2024-01-04 12:23:39 -0600

    Arm and FieldCentric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -1,36 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+
 
 public abstract class Arm extends LinearOpMode {
     DcMotor arm;
 
     public void runOpMode() throws InterruptedException {
-        int armUpPosition = 150;
+        int armUpPosition = 800;
         int armDownPosition = 0;
+
+        double position = arm.getCurrentPosition();
+        double desiredPosition = arm.getTargetPosition();
 
         arm = hardwareMap.dcMotor.get("Arm");
 
         arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-       arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
 
         while(opModeIsActive()) {
-            if(gamepad2.cross) {
+            if(gamepad1.circle && position < armUpPosition) {
+                arm.setTargetPosition(armUpPosition);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                arm.setPower(0.2);
+            }
+            else if (gamepad2.square && position > armDownPosition) {
                 arm.setTargetPosition(armDownPosition);
                 arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                arm.setPower(0.5);
+                arm.setPower(-0.2);
             }
-
-            double position = arm.getCurrentPosition();
-            double desiredPosition = arm.getTargetPosition();
+            else {
+               arm.setPower(0);
+            }
 
             telemetry.addData("Encoder Position", position);
             telemetry.addData("Desired Position", desiredPosition);
             telemetry.update();
         }
-
     }
 }
